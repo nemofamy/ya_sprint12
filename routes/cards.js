40ddb314ec1e0e41cc151a81cbe21c9acc4d1573
Tema Konovalov <konovalovtema@mail.ru>
2019-11-08 22:24:06 +0300

    i just use require instead of ds.readFile

--- routes/cards.js
+++ routes/cards.js
@@ -2,18 +2,9 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 
-const sendCards = (req, res) => {
-  const dataPath = path.join(__dirname, '../data/cards.json');
-  fs.readFile(dataPath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      // eslint-disable-next-line no-console
-      console.log('Что-то пошло не так с карточками');
-      // eslint-disable-next-line no-console
-      console.log(err);
-      return;
-    }
-    res.send(JSON.parse(data));
-  });
-};
-router.get('/cards', sendCards);
+const cards_data = require('../data/cards');
+
+router.get('/cards', (req, res) => {
+  res.send(cards_data);
+});
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,46 +2,24 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 
-// здесь по аналогии с карточками
-const sendUsers = (req, res) => {
-  const dataPath = path.join(__dirname, '../data/users.json');
-  fs.readFile(dataPath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      // eslint-disable-next-line no-console
-      console.log('Что-то пошло не так с пользователями');
-      // eslint-disable-next-line no-console
-      console.log(err);
-      return;
-    }
-    res.send(JSON.parse(data));
-  });
-};
+const users_data = require('../data/users');
 
 const sendUserById = (req, res) => {
-  const dataPath = path.join(__dirname, '../data/users.json');
-  fs.readFile(dataPath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      // eslint-disable-next-line no-console
-      console.log('Что-то пошло не так с пользователем по ID');
-      // eslint-disable-next-line no-console
-      console.log(err);
-      return;
-    }
-    const users = JSON.parse(data);
     // перебираем пользователей и сравниваем их id с запрошенным
-    for (let i = 0; i < users.length; i += 1) {
+    for (let i = 0; i < users_data.length; i += 1) {
       // eslint-disable-next-line no-underscore-dangle
-      if (users[i]._id === req.params.id) {
-        res.send(users[i]);
+      if (users_data[i]._id === req.params.id) {
+        res.send(users_data[i]);
         return; // если нашли - выходим из перебора
       }
     }
     // если не нашли - шлем 404
     res.status(404).send({ message: 'Нет пользователя с таким id' });
-  });
 };
 
-router.get('/users', sendUsers);
+router.get('/users', (req, res) => {
+  res.send(users_data);
+});
 router.get('/users/:id', sendUserById);
 
 // на случай непонятного запроса
