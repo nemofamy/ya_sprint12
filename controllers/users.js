a6a242e89a2d07eca292b7e340061f7b9c544463
Tema Konovalov <konovalovtema@mail.ru>
2019-11-12 22:37:43 +0300

    bug fixes

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,13 @@ module.exports.getUsers = (req, res) => {
 module.exports.getUserById = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
-    .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+      } else {
+        res.status(404).send({ message: 'Пользователь не найден' });
+      }
+    })
     .catch((err) => res.status(404).send({ message: err.message }));
 };
 
